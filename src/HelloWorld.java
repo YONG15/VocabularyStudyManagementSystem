a8ada0b3ae4df90904ac99d417f949a18a4f92c5
YONG <YONG@DESKTOP-O4AE1K3>
2020-03-17 23:58:59 +0900

    Create a menu

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -8,7 +8,7 @@ public class HelloWorld {
 		System.out.println(" 3. Edit student");
 		System.out.println(" 4. View student");
 		System.out.println(" 5. Exit");
-		System.out.println("Select one number between 1 - 5 :");
+		System.out.println("  Select one number between 1 - 5 :");
 
 	}
 
