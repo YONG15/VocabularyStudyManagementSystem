9a8ba1100056e722e78542de867236e23d121c5b
YONG <YONG@DESKTOP-O4AE1K3>
2020-03-17 23:52:34 +0900

    Create a menu

---
+++ src/HelloWorld.java
@@ -0,0 +1,15 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("*** Student Management System Menu ***");
+		System.out.println(" 1. Add student");
+		System.out.println(" 2. Delete student");
+		System.out.println(" 3. Edit student");
+		System.out.println(" 4. View student");
+		System.out.println(" 5. Exit");
+		System.out.println("Select one number between 1 - 5 :");
+
+	}
+
+}
