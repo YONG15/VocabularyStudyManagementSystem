9cf1bdda5ac6907cc4ef2e7a222082d207a99698
YONG <YONG@DESKTOP-O4AE1K3>
2020-03-18 22:27:40 +0900

    Edit MenuManager

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -3,10 +3,10 @@ public class MenuManager {
 
 	public static void main(String[] args) {
 		System.out.println("*** Vocabulary Study Management System Menu ***");
-		System.out.println(" 1. Add student");
-		System.out.println(" 2. Delete student");
-		System.out.println(" 3. Edit student");
-		System.out.println(" 4. View student");
+		System.out.println(" 1. Add vocabulary");
+		System.out.println(" 2. Delete vocabulary");
+		System.out.println(" 3. Edit vocabulary");
+		System.out.println(" 4. View vocabulary");
 		System.out.println(" 5. Exit");
 		System.out.println("Select one number between 1 - 5 :");
 
