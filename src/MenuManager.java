e47c8f18c3bb1cd6a9cc5ce82745c6159fac5451
YONG <YONG@DESKTOP-O4AE1K3>
2020-03-18 21:59:06 +0900

    create a menu

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -2,13 +2,13 @@
 public class MenuManager {
 
 	public static void main(String[] args) {
-		System.out.println("*** Student Management System Menu ***");
+		System.out.println("*** Vocabulary Study Management System Menu ***");
 		System.out.println(" 1. Add student");
 		System.out.println(" 2. Delete student");
 		System.out.println(" 3. Edit student");
 		System.out.println(" 4. View student");
 		System.out.println(" 5. Exit");
-		System.out.println("  Select one number between 1 - 5 :");
+		System.out.println("Select one number between 1 - 5 :");
 
 	}
 
